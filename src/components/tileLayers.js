9d9ff0fc7ebb0045499376e4b6b59f0f98d7c780
dupont <gilldupont@gmail.com>
2021-05-31 23:54:12 +0200

    correction d'un bug

--- src/components/tileLayers.js
+++ src/components/tileLayers.js
@@ -1,8 +1,21 @@
 import L from 'leaflet'
+import {mapboxToken} from '../../secret'
 
 //couches du geoportail
 const CLEFIGN = "choisirgeoportail"
-const mapboxAccessToken = 'pk.eyJ1IjoiZ2lsbGVzODQ3NSIsImEiOiJjazdmcmtuM2YwNWZrM2VuNjlrbnNldGI3In0.NVN_OrsfDaW6RfsQzwY4jg';
+
+
+const stylesMap = {
+    streets: 'mapbox/streets-v11',
+    outdoors: 'mapbox/outdoors-v11',
+    light: 'mapbox/light-v10',
+    dark: 'mapbox/dark-v10',
+    sat: 'mapbox/satellite-v9',
+    satstreets: 'mapbox/satellite-streets-v11',
+    navday: 'mapbox/navigation-day-v1',
+    navnight: 'mapbox/navigation-night-v1'
+
+}
 
 export const IgnTypes={
     IgnPlan:{
@@ -32,12 +45,12 @@ export const IgnLayer = ({techName, format}) => L.tileLayer(
         tileSize: 256 // les tuiles du Géooportail font 256x256px
     }
 )
-
+//couche de mapbox attention de bien créer un fichier secret.js et d'y mettre l'access token à récupérer sur le site https://account.mapbox.com/
 export const mapboxLayer = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/outdoors-v11',
+    id: stylesMap.outdoors,
     tileSize: 512,
     zoomOffset: -1,
-    accessToken: mapboxAccessToken
+    accessToken: mapboxToken
 })
